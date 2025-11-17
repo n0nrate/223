8004696f93046ebdcbad64e06e5bec853bde7a00
n0nrate <53004800+n0nrate@users.noreply.github.com>
2025-11-17 21:27:53 +0300

    Update electron.js

--- client/public/electron.js
+++ client/public/electron.js
@@ -1,66 +1,35 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const { spawn } = require('child_process');
-
-let mainWindow;
-let serverProcess;
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 function createWindow() {
-  // Start the backend server
-  const isDev = process.env.NODE_ENV === 'development';
-  const serverPath = isDev
-    ? path.join(__dirname, '..', 'server', 'index.js')
-    : path.join(process.resourcesPath, 'server', 'index.js');
-  const serverCwd = isDev
-    ? path.join(__dirname, '..', 'server')
-    : path.join(process.resourcesPath, 'server');
+  const isDev = process.env.NODE_ENV === "development";
 
-  serverProcess = spawn('node', [serverPath], {
-    cwd: serverCwd,
-    stdio: 'inherit'
-  });
-
-  // Create the browser window
-  mainWindow = new BrowserWindow({
+  const win = new BrowserWindow({
     width: 1200,
     height: 800,
     webPreferences: {
-      nodeIntegration: false,
       contextIsolation: true,
-    },
-  });
-
-  // Load the React app
-  const startUrl = isDev
-    ? 'http://localhost:3000'
-    : `file://${path.join(__dirname, '../build/index.html')}`;
-  mainWindow.loadURL(startUrl);
-
-  // Open DevTools in development
-  if (isDev) {
-    mainWindow.webContents.openDevTools();
-  }
-
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-    if (serverProcess) {
-      serverProcess.kill();
+      preload: path.join(__dirname, "preload.js")
     }
   });
+
+  // Загружаем React билд
+  const startUrl = isDev
+    ? "http://localhost:3000"
+    : `file://${path.join(__dirname, "../build/index.html")}`;
+
+  win.loadURL(startUrl);
+
+  if (isDev) win.webContents.openDevTools();
 }
 
 app.whenReady().then(createWindow);
 
-app.on('window-all-closed', () => {
-  if (serverProcess) {
-    serverProcess.kill();
-  }
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
