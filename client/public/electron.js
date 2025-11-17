ed20c46ecaef63de46590f53f65c1db1d3b6c25b
n0nrate <stressedout67@mail.ru>
2025-11-18 00:38:58 +0300

    add electron.js

---
+++ client/public/electron.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const isDev = process.env.NODE_ENV === "development";
+
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      contextIsolation: true,
+      preload: path.join(__dirname, "preload.js")
+    }
+  });
+
+  const startUrl = isDev
+    ? "http://localhost:3000"
+    : `file://${path.join(__dirname, "../build/index.html")}`;
+
+  win.loadURL(startUrl);
+
+  if (isDev) win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
